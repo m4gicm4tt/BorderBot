48fcd552da8099b6d411882de71c677f59f10e32
m4gicm4tt <m4gicm4tt@live.com>
2012-09-30 12:31:44 -0400

    first commit

---
+++ src/org/m4gicm4tt/BorderBot.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.m4gicm4tt;
+
+/**
+ * @author Matthew
+ *
+ */
+public class BorderBot {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
