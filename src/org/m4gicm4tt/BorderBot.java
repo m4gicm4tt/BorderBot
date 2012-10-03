a520f89c2078fa36f5703bb43d0eedeab6f9eb9f
m4gicm4tt <m4gicm4tt@live.com>
2012-10-03 00:11:55 -0400

    gets to part where you ctrl-v code in
    must be run in a window

--- src/org/m4gicm4tt/BorderBot.java
+++ src/org/m4gicm4tt/BorderBot.java
@@ -5,6 +5,11 @@ package org.m4gicm4tt;
 
 import java.awt.AWTException;
 import java.awt.Robot;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * @author Matthew
@@ -12,30 +17,162 @@ import java.awt.Robot;
  */
 public class BorderBot {
 
-	// private enum state{start, launch, waitForPlay, waitForMenu, goToShift}
-	public static final String ver = "1.0"; 
+	private enum state {
+		stop, init, idle, start, goToShift, inShift, enterShiftCode, waitForOk
+	}
+
+	public state myState = state.idle;
+	public static final String ver = "1.0";
+	public File imageToCompare = null;
+	public String execPath = "";
 
 	/**
 	 * @param args
+	 * 
+	 *            public static void main(String[] args) { ImageSearch iSearch =
+	 *            new ImageSearch(); int[] result = iSearch.findPicAInPicB(
+	 *            iSearch.getImage(System.getProperty("user.dir") +
+	 *            "\\src\\org\\m4gicm4tt\\image.bmp"), iSearch.getScreenshot(),
+	 *            20); Robot myRobot;
+	 * 
+	 *            try { myRobot = new Robot(); myRobot.mouseMove(result[0],
+	 *            result[1]); //
+	 *            myRobot.mousePress(InputEvent.getMaskForButton(MouseEvent
+	 *            .BUTTON1)); // Thread.sleep(1000); //
+	 *            myRobot.mouseRelease(InputEvent
+	 *            .getMaskForButton(MouseEvent.BUTTON1)); } catch (AWTException
+	 *            e) { e.printStackTrace(); } System.out.println("result " +
+	 *            result[0] + ", " + result[1]); }
 	 */
-	public static void main(String[] args) {
+	public void setImageToCompare(File f) {
+		imageToCompare = f;
+	}
+
+	public void setExecPath(String s) {
+		execPath = s;
+	}
+
+	public void runMachine() {
+		while (myState != state.stop) {
+			stateMachine();
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void stopMachine() {
+		myState = state.idle;
+	}
+
+	public void startMachine() {
+		myState = state.init;
+	}
+
+	private boolean findPic(String s) {
+		int[] result = findPicCoords(s);
+		return ((result[0] != -1) && (result[1] != -1));
+	}
+
+	private int[] findPicCoords(String s) {
 		ImageSearch iSearch = new ImageSearch();
 		int[] result = iSearch.findPicAInPicB(
 				iSearch.getImage(System.getProperty("user.dir")
-						+ "\\src\\org\\m4gicm4tt\\image.bmp"),
-						iSearch.getScreenshot(), 20);
-		Robot myRobot;
-
-		try {
-			myRobot = new Robot();
-			myRobot.mouseMove(result[0], result[1]);
-			// myRobot.mousePress(InputEvent.getMaskForButton(MouseEvent.BUTTON1));
-			// Thread.sleep(1000);
-			// myRobot.mouseRelease(InputEvent.getMaskForButton(MouseEvent.BUTTON1));
-		} catch (AWTException e) {
-			e.printStackTrace();
-		}
+						+ "\\src\\org\\m4gicm4tt\\" + s),
+				iSearch.getScreenshot(), 20);
 		System.out.println("result " + result[0] + ", " + result[1]);
+		return result;
 	}
 
+	public void stateMachine() {
+		Robot myRobot;
+		// ImageSearch iSearch;
+		// int[] result;
+
+		switch (myState) {
+		case init:
+			Runtime rt = Runtime.getRuntime();
+			try {
+				Process proc = rt.exec(execPath);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			myState = state.start;
+			break;
+		case start:
+			try {
+				myRobot = new Robot();
+				myRobot.mouseMove(200, 50);
+				myRobot.mousePress(InputEvent
+						.getMaskForButton(MouseEvent.BUTTON1));
+				Thread.sleep(500);
+				myRobot.mouseRelease(InputEvent
+						.getMaskForButton(MouseEvent.BUTTON1));
+			} catch (AWTException | InterruptedException e) {
+				e.printStackTrace();
+			}
+			if (findPic("mainMenu.bmp"))
+				myState = state.goToShift;
+			break;
+		case goToShift:
+			try {
+				myRobot = new Robot();
+				for (int i = 0; i < 6; i++) {
+					myRobot.keyPress(KeyEvent.VK_DOWN);
+					Thread.sleep(100);
+					myRobot.keyRelease(KeyEvent.VK_DOWN);
+					Thread.sleep(100);
+				}
+				myRobot.keyPress(KeyEvent.VK_ENTER);
+				Thread.sleep(100);
+				myRobot.keyRelease(KeyEvent.VK_ENTER);
+				Thread.sleep(100);
+			} catch (AWTException | InterruptedException e) {
+				e.printStackTrace();
+			}
+			myState = state.inShift;
+			break;
+		case inShift:
+			if (findPic("shiftMenu.bmp")) {
+				try {
+					myRobot = new Robot();
+					myRobot.keyPress(KeyEvent.VK_ENTER);
+					Thread.sleep(500);
+					myRobot.keyRelease(KeyEvent.VK_ENTER);
+					Thread.sleep(500);
+				} catch (AWTException | InterruptedException e) {
+					e.printStackTrace();
+				}
+				myState = state.enterShiftCode;
+			}
+			break;
+		case enterShiftCode:
+			int[] res = findPicCoords("shiftCode.bmp");
+			if (((res[0] != -1) && (res[1] != -1))) {
+				try {
+					myRobot = new Robot();
+					myRobot.mouseMove(res[0], res[1]);
+					Thread.sleep(100);
+					myRobot.mouseMove(res[0]-1, res[1]);
+					myRobot.mousePress(InputEvent
+							.getMaskForButton(MouseEvent.BUTTON1));
+					Thread.sleep(500);
+					myRobot.mouseRelease(InputEvent
+							.getMaskForButton(MouseEvent.BUTTON1));
+				} catch (AWTException | InterruptedException e) {
+					e.printStackTrace();
+				}
+				myState = state.waitForOk;
+			}
+			break;
+		case waitForOk:
+
+			myState = state.idle;
+			break;
+		}
+	}
 }
--- src/org/m4gicm4tt/BorderGUI.java
+++ src/org/m4gicm4tt/BorderGUI.java
@@ -21,10 +21,17 @@ import java.awt.GridLayout;
 import java.awt.Window.Type;
 import java.awt.Frame;
 import javax.swing.JFileChooser;
+import javax.swing.border.EtchedBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
-public class BorderGUI {
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
+public class BorderGUI implements ActionListener {
+
+	private static BorderBot bBot;
 	private JFrame frmBorderbot;
+	private JButton btnNewButton;
 
 	/**
 	 * Launch the application.
@@ -40,6 +47,9 @@ public class BorderGUI {
 				}
 			}
 		});
+
+		bBot = new BorderBot();
+		bBot.runMachine();
 	}
 
 	/**
@@ -56,52 +66,101 @@ public class BorderGUI {
 		frmBorderbot = new JFrame();
 		frmBorderbot.setResizable(false);
 		frmBorderbot.setTitle("BorderBot");
-		BorderLayout borderLayout = (BorderLayout) frmBorderbot.getContentPane().getLayout();
+		BorderLayout borderLayout = (BorderLayout) frmBorderbot
+				.getContentPane().getLayout();
 		borderLayout.setVgap(5);
 		borderLayout.setHgap(5);
-		frmBorderbot.setBounds(100, 100, 450, 300);
+		frmBorderbot.setBounds(100, 100, 448, 371);
 		frmBorderbot.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		JPanel panelTop = new JPanel();
 		frmBorderbot.getContentPane().add(panelTop, BorderLayout.NORTH);
-		
-		JLabel lblNewLabel = new JLabel("BorderBot v"+BorderBot.ver);
+
+		JLabel lblNewLabel = new JLabel("BorderBot v" + BorderBot.ver);
 		panelTop.add(lblNewLabel);
 		lblNewLabel.setBorder(new EmptyBorder(2, 2, 2, 2));
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 35));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		
-		JPanel panelSouth = new JPanel();
-		panelSouth.setBorder(new EmptyBorder(0, 5, 5, 5));
-		frmBorderbot.getContentPane().add(panelSouth, BorderLayout.SOUTH);
-		panelSouth.setLayout(new BorderLayout(0, 0));
-		
+
+		JPanel panelCenter = new JPanel();
+		panelCenter.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null,
+				null, null));
+		frmBorderbot.getContentPane().add(panelCenter, BorderLayout.CENTER);
+		panelCenter.setLayout(new BorderLayout(0, 0));
+
+		JPanel panel = new JPanel();
+		panel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		panelCenter.add(panel, BorderLayout.NORTH);
+		panel.setLayout(new BorderLayout(0, 0));
+
+		btnNewButton = new JButton("Set Path");
+		btnNewButton.addActionListener(this);
+		panel.add(btnNewButton, BorderLayout.NORTH);
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 35));
+
 		JTextArea txtrTesting = new JTextArea();
+		panel.add(txtrTesting, BorderLayout.CENTER);
 		txtrTesting.setBackground(Color.BLACK);
 		txtrTesting.setText("testing");
 		txtrTesting.setForeground(Color.GREEN);
 		txtrTesting.setBorder(new LineBorder(new Color(0, 0, 0)));
-		panelSouth.add(txtrTesting);
-		txtrTesting.setRows(5);
+		txtrTesting.setRows(10);
 		txtrTesting.setEditable(false);
-		
-		JPanel panelCenter = new JPanel();
-		panelCenter.setBorder(new EmptyBorder(5, 5, 0, 5));
-		frmBorderbot.getContentPane().add(panelCenter, BorderLayout.CENTER);
-		panelCenter.setLayout(new GridLayout(0, 3, 0, 0));
-		
-		JButton btnNewButton = new JButton("Start");
-		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 35));
-		panelCenter.add(btnNewButton);
-		
-		JLabel lblCurrentPath = new JLabel("Current Path");
-		lblCurrentPath.setHorizontalTextPosition(SwingConstants.CENTER);
-		lblCurrentPath.setHorizontalAlignment(SwingConstants.CENTER);
-		lblCurrentPath.setAlignmentX(Component.CENTER_ALIGNMENT);
-		panelCenter.add(lblCurrentPath);
-		
-		JButton btnNewButton_1 = new JButton("New button");
-		panelCenter.add(btnNewButton_1);
+
+		JPanel panelSouth = new JPanel();
+		panelSouth.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null,
+				null, null));
+		frmBorderbot.getContentPane().add(panelSouth, BorderLayout.SOUTH);
+		panelSouth.setLayout(new BorderLayout(0, 0));
+
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new EmptyBorder(5, 5, 5, 5));
+		panelSouth.add(panel_1, BorderLayout.SOUTH);
+		panel_1.setLayout(new BorderLayout(0, 0));
+
+		JLabel lblNewLabel_1 = new JLabel("Path:");
+		panel_1.add(lblNewLabel_1, BorderLayout.WEST);
+		lblNewLabel_1.setBorder(new EmptyBorder(2, 2, 2, 2));
+
+		JLabel lblNewLabel_2 = new JLabel("Not Set");
+		panel_1.add(lblNewLabel_2, BorderLayout.CENTER);
+		lblNewLabel_2.setBorder(new EmptyBorder(2, 2, 2, 2));
+	}
+
+	public void actionPerformed(ActionEvent arg0) {
+		System.out.println(btnNewButton.getText());
+		if (btnNewButton.getText().equals("Set Path")) {
+			JFileChooser jFile = new JFileChooser("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Borderlands 2\\Binaries\\Win32\\");
+			FileNameExtensionFilter filter = new FileNameExtensionFilter(
+					"executibles", "exe");
+			jFile.setFileFilter(filter);
+			int returnVal = jFile.showOpenDialog(btnNewButton);
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+				System.out.println("You chose to open this file: "
+						+ jFile.getSelectedFile().getAbsolutePath());
+
+				bBot.setExecPath(jFile.getSelectedFile().getAbsolutePath());
+			}
+			EventQueue.invokeLater(new Runnable() {
+				public void run() {
+					btnNewButton.setText("Start");
+				}
+			});
+		} else if (btnNewButton.getText().equals("Start")) {
+			bBot.startMachine();
+			EventQueue.invokeLater(new Runnable() {
+				public void run() {
+					btnNewButton.setText("Stop");
+				}
+			});
+		} else if (btnNewButton.getText().equals("Stop")) {
+			bBot.stopMachine();
+			EventQueue.invokeLater(new Runnable() {
+				public void run() {
+					btnNewButton.setText("Start");
+				}
+			});
+		}
 	}
 
 }
